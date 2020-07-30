3091e50c8f553b1acd2824d922b27bcc9f7dc27b
CanerSungur <caner.sokur@outlook.com>
2020-07-30 15:08:57 +0300

    Create Player.cs

---
+++ Scripts/Player.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform target;
+
+    [Header("Unity Setup Fields")]
+    public string enemyTag = "Enemy";
+
+    public float turnSpeed = 5f;
+
+    [Header("Bullet Info")]
+    public GameObject bulletPrefab;
+    public float fireRate;
+    private float fireCountdown = 0;
+    public Transform firePoint;
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null)
+        {
+
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        LockOnTarget();
+
+        if (fireCountdown <= 0)
+        {
+            Shoot();
+            fireCountdown = 1f / fireRate;
+        }
+        fireCountdown -= Time.deltaTime;
+    }
+
+    private void LockOnTarget()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, turnSpeed * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletGameObject = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGameObject.GetComponent<Bullet>();
+
+        if (bullet != null)
+        {
+            bullet.SeekTarget(target);
+        }
+    }
+}
